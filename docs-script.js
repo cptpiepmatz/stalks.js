ff68a92c44bfc00eeb9d476cd62807cb38f7b088
Piepmatz <derPiepmatz@users.noreply.github.com>
2020-07-01 21:24:13 +0200

    Styled a bit more the Docs

---
+++ docs-script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let faviconElement = document.createElement("LINK");
+faviconElement.rel = "shortcut icon";
+faviconElement.href = "logo.png";
+faviconElement.type = "image/png";
+document.head.appendChild(faviconElement);
